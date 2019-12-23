d38768707c3aa543d27ad165950c1d45c5de239d
Nathan Barber <nathanbarber6@gmail.com>
2019-12-22 21:28:30 -0500

    Added node linting to root/server

--- server/contentful/getPost.js
+++ server/contentful/getPost.js
@@ -1,12 +1,12 @@
 module.exports = contentfulClient => (req, res) => {
-    const postType = req.query.type == "custom"
-        ? "customPosting"
-        : "posting";
-    contentfulClient.getEntries({ "content_type": postType })
-        .then(res => JSON.stringify(res))
-        .then(data => res.status(200).send(data))
-        .catch(err => res.status(500).send(JSON.stringify({
-            success: false,
-            error: err
-        })));
-}
+	const postType = req.query.type == 'custom'
+		? 'customPosting'
+		: 'posting';
+	contentfulClient.getEntries({ 'content_type': postType })
+		.then(res => JSON.stringify(res))
+		.then(data => res.status(200).send(data))
+		.catch(err => res.status(500).send(JSON.stringify({
+			success: false,
+			error: err
+		})));
+};
--- server/stripe/handleCharge.js
+++ server/stripe/handleCharge.js
@@ -1,17 +1,17 @@
 module.exports = stripeClient => (req, res) => {
-    const { token, description, amt } = req.body;
-    stripeClient.charges.create({
-            amount: amt,
-            currency: 'usd',
-            description: description,
-            source: token
-        })
-        .then(data => res.status(200).send({
-            success: true,
-            body: data
-        }))
-        .catch(err => res.status(500).send({
-            success: false,
-            error: err
-        }));
-}
+	const { token, description, amt } = req.body;
+	stripeClient.charges.create({
+		amount: amt,
+		currency: 'usd',
+		description: description,
+		source: token
+	})
+		.then(data => res.status(200).send({
+			success: true,
+			body: data
+		}))
+		.catch(err => res.status(500).send({
+			success: false,
+			error: err
+		}));
+};
